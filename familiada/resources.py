2486af8aa9415eb2c8cdc1f7ca24bafef032e995
wonszbagienny <matkub19@gmail.com>
2020-07-18 14:11:09 +0200

    Update

---
+++ familiada/resources.py
@@ -0,0 +1,109 @@
+Colors = [
+    'Porzeczkowi', 'Śliwkowi', 'Musztardowi', 'Kalafiorowi', 'Niebiescy', 'Miętowi', 'Pomarańczowi', 'Karmazynowi', 'Kobaltowi',
+    'Limonkowi', 'Harnaś Rules', 'Żubrówka Team', 'Fani Erepublik', 'Plato To Penis'    
+]
+
+Questions = [
+    "Jakie zwierzę można spotkać podczas spaceru przez pola?",
+    "Co kupujemy w tubce?",
+    "Podaj przykład komiksowego superbohatera.",
+    "Sport w którym walczymy z przeciwnikiem, ale go nie dotykamy.",
+    "Wymień przykład emocji.",
+    "Państwo, którego nazwa kończy się na 'nia'.",
+    "Wymień cechę pani Justyny.",
+    "Co jest w pociągu i w domu?",
+    "Co potrafią robić ptaki?",
+    "Część garderoby na literę 's'.",
+    "Co robimy w pociągu?",
+    "Co można robić samemu w domu?",
+    "Owoc z jedną pestką.",
+    "Popularne nazwisko.",
+    "Na czym możesz siedzieć? Podaj odpowiedź w mianowniku -.^.",
+    "Zwierzę domowe.",
+    "Miejski ptak.",
+    "Co zabieramy na plażę?",
+    "Leży na biurku.",
+    "Co rozwiązujemy chociaż tego nie zawiązujemy?",
+    "Co napełnia się gazem?"
+]
+
+Answers = [
+    ['krowę', 'zająca', 'sarnę', 'bociana'],
+    ['pastę do zębów', 'krem do rąk', 'klej'],
+    ['superman', 'spiderman', 'batman', 'ironman'],
+    ['tenis', 'ping-pong', 'siatkówka', 'szachy'],
+    ['strach', 'złość', 'smutek', 'radość', 'zaskoczenie'],
+    ['dania', 'hiszpania', 'japonia', 'estonia', 'rumunia'],
+    ['inteligentna', 'piękna', 'pracowita', 'kreatywna'],
+    ['okna', 'drzwi', 'toaleta', 'fotele', 'ludzie'],
+    ['srać', 'latać', 'śpiewać', 'znosić jaja'],
+    ['spodnie', 'sweter', 'sukienka', 'spódnica', 'szalik'],
+    ['czytamy', 'śpimy', 'słuchamy muzyki'],
+    ['walić konia', 'zakapturzać mnicha', 'czochrać bobra', 'ślizgać pingwina'],
+    ['śliwka', 'wiśnia', 'morela'],
+    ['kowalski', 'nowak', 'wiśniewski'],
+    ['dupa', 'krzesło', 'fotel', 'kanapa'],
+    ['kot', 'pies', 'chomik'],
+    ['gołąb', 'wróbel', 'sikorka'],
+    ['ręcznik', 'książkę', 'napój', 'krem do opalania'],
+    ['laptop', 'zeszyt', 'długopis', 'marihuana', 'szlugi'],
+    ['zagadkę', 'krzyżówkę', 'zadanie', 'równanie', 'problem'],
+    ['butlę', 'zapalniczkę', 'bak', 'balon']
+]
+
+Hints = [
+    ['mleko daje','długie uszy ma', 'zgrabna i skoczna', 'jest biały'],
+    ['do pielęgnacji jamy ustnej', 'do pielęgnacji łapek', 'wącha go maxiu'],
+    ['super czlowiek', 'czlowiek pajonk', 'czlowiek nietopemsz', 'czlowiek rzelazo'],
+    ['pilka sie odbija od ziemi przez siatke', 'pilka sie odbija od stolu przez siatke', 'pilke sie odbija przez siatke', 'bardzo lubil je Samuel Beckett'],
+    ['Dżem - List do M', 'Kazik - Prosto', 'Fogg - Ta ostatnia niedziela', 'Krzysztof Krwaczyk - Parostatek', 'Manaam - Lipstick on the glass XD'],
+    ['i napoje', 'visca el barca', 'sushi mnie', 'chcieliby byc wikingami ale nie naleza do Skandynawii', 'jedza tam gruz'],
+    ['szybko sie uczy', 'bardzo milo na nia patrzec', 'chetnie podejmuje sie pracy', 'potrafi myslec tworczo'],
+    ['widac przez nie swiat', 'mozna przez nie wyjsc na swat', 'krol chodzi tam piechota', 'moga byc bujane choc nie w pociagach', '7 miliardow ich lazi a bedzie wiecej'],
+    ['robic po katach', 'unosic sie w powietrzu', 'zaklocac cisze nocna', 'produkowac materialy na jajecznice'],
+    ['na nogi', 'na tulow i rece', 'na cale cialo dla baby', 'myla ja z haslem nr 3', 'na szyje'],
+    ['patrzymy w ksionshke', 'ucinamy sobie drzemke', 'odpalamy Spotify'],
+    ['styl na rolnika', 'styl na mnicha', 'styl na lesniczego', 'styl na Skippera'],
+    ['na sliwowice', 'na wisniowke', 'egzotyczne'],
+    ['kowal', 'nowy w wiosce', 'lubi wisnie'],
+    ['siedziesz na niej xD', 'moze byc skladane', 'moze byc bujany', 'mozna sie tez na niej kimnac'],
+    ['miau miau', 'hau hau', 'ma bardzo pojemne policzki'],
+    ['obsrywa mi parapet', 'elemel(ek)', 'rich bitch [bogata jest]'],
+    ['coby sie wysuszyc i na nim polozyc', 'coby sobie poczytac', 'coby sie napic bo goraco', 'coby sie posmarowac to skora sie nie spiecze'],
+    ['przenosny konkuter', 'miejsce na notatki', 'cos do robienia notatek', '420', '20 w paczce, przynajmniej na start'],
+    ['zadaje ja sfinks', 'lubie rozwiazac jedna na kiblu', 'jest ich duzo w podreczniku do matematyki', '... matematyczne', 'jak masz jakis to zbierasz wpierdol'],
+    ['do kuchenki gazowej', 'do odpalania szluga', 'w samochodzie', 'moze byc meteorologiczny']
+]
+
+Points = [
+    [47, 22, 19, 12],
+    [58, 26, 16],
+    [47, 22, 19, 12],
+    [47, 22, 19, 12],
+    [39, 19, 17, 15, 10],
+    [39, 19, 17, 15, 10],
+    [47, 22, 19, 12],
+    [39, 19, 17, 15, 10],
+    [47, 22, 19, 12],
+    [39, 19, 17, 15, 10],
+    [58, 26, 16],
+    [47, 22, 19, 12],
+    [58, 26, 16],
+    [58, 26, 16],
+    [47, 22, 19, 12],
+    [58, 26, 16],
+    [47, 22, 19, 12],
+    [58, 26, 16],
+    [58, 26, 16],
+    [47, 22, 19, 12],
+    [39, 19, 17, 15, 10],
+    [39, 19, 17, 15, 10],
+    [47, 22, 19, 12]
+]
+
+Puns = [
+    'Wiecie dlaczego bocian stoi na jednej nodze?\nBo jakby ja podniósł to by sie na dziób wywalił.',
+    'Skąd pochodzi random.w?\nZ Bytomia.',
+    'Po czym jest maxiu?\nPo krokodylu.',
+    'Dlaczego w Białymstoku nie nosi sie jajek na święcenie?\nBo Uzury je Zezre.'
+]
